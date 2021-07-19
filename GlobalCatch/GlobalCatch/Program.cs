aaf49db550e298920738d792edf1ffe149304470
Sabotage84 <sabotage84@mail.ru>
2021-07-19 22:45:58 +0300

    ADD global catch

---
+++ GlobalCatch/GlobalCatch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GlobalCatch
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(GlobalEx);
+            Application.Run(new Form1());
+        }
+
+        private static void GlobalEx(object sender, ThreadExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message);
+        }
+    }
+}
