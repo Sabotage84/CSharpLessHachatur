91c4d593c3d3ab440fc9a9154fd0efdb4f49f755
Sabotage84 <sabotage84@mail.ru>
2021-07-19 22:28:24 +0300

    ADD localisation via dictionary

---
+++ LocalisationViaDictionary/LocalisationViaDictionary/LocalisationT.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalisationViaDictionary
+{
+    public static class LocalisationT
+    {
+        public static string Translation (string line)
+        {
+            return russian[line]; //Нужен выбор языка, проверка на отсутствие в словаре итд, но мысль такая.
+        }
+
+        public static Dictionary<string, string> russian = new Dictionary<string, string>
+        {
+            ["Hello"] = "Привет!",
+            ["Good Bye"] = "Пока!",
+            ["Word"] = "Слово"
+        };
+
+        public static Dictionary<string, string> english = new Dictionary<string, string>
+        {
+            ["Hello"] = "Hi!",
+            ["Good Bye"] = "Bye!",
+            ["Word"] = "Word"
+        };
+    }
+}
