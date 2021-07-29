322a1918b641b688388edfac01d936156e716b45
Sabotage84 <sabotage84@mail.ru>
2021-07-29 17:38:24 +0300

    ADD tuple

---
+++ Tuple/Tuple/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tuple
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = Meth1(true).Item2;
+        }
+
+        Tuple<int, string> Meth1(bool b)
+        {
+            return new Tuple<int, string>(123, "EHU!");
+        }
+    }
+}
