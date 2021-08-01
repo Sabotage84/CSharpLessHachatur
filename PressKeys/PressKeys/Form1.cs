cbf7848db43b5a67d3d1110adcddff4905ee3e7e
Sabotage84 <sabotage84@mail.ru>
2021-08-01 16:03:52 +0300

    ADD лун зкуыы

---
+++ PressKeys/PressKeys/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PressKeys
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //KeyPreview должен быть true
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue==(char)Keys.Enter)
+            {
+                button1_Click(button1, null);//вызываем нажатие кнопки
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text;
+        }
+    }
+}
