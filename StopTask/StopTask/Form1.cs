f0636a00acd777d2e827e3f891f16d5b0ff004c3
Sabotage84 <sabotage84@mail.ru>
2021-08-02 16:48:48 +0300

    ADD Task stop

---
+++ StopTask/StopTask/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StopTask
+{
+    public partial class Form1 : Form
+    {
+        bool need = true;
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+            label1.Click += (s, e) => { need = false; };
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        async void button1_Click(object sender, EventArgs e)
+        {
+            await Task.Run(async () =>
+            {
+                while (need)
+                {
+                    label1.Text += "1";
+                    await Task.Delay(200);
+                }
+            });
+        }
+    }
+}
