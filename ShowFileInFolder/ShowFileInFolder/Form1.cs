e55ce54cc0e5c68ae9dd1acb328af8489e4fba72
Sabotage84 <sabotage84@mail.ru>
2021-08-03 16:36:49 +0300

    Add show file in folder

---
+++ ShowFileInFolder/ShowFileInFolder/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShowFileInFolder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = @"C:\projects\CSharpLessHachatur\.gitignore";
+            CMD(path);
+        }
+
+        void CMD (string path)
+        {
+            Process.Start(new ProcessStartInfo { FileName = "explorer", Arguments = $"/n, /select, {path}" });
+        }
+    }
+}
