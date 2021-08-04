b050cdf11b9f7db54cd3af3610cc4678eebeaa42
Sabotage84 <sabotage84@mail.ru>
2021-08-04 16:34:23 +0300

    Add GetCommandLineArgs

---
+++ GetCommandLineArgs/GetCommandLineArgs/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetCommandLineArgs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Environment.GetCommandLineArgs().ToList().ForEach(x=>
+            {
+                if (x.EndsWith(@"/key"))
+                    BackColor=Color.Red;
+            }
+            );
+        }
+    }
+}
