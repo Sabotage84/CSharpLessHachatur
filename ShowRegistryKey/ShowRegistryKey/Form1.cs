ac07f46d31d44544cf2ea32f227f19bf6d2402f1
Sabotage84 <sabotage84@mail.ru>
2021-08-06 17:00:46 +0300

    Add reg show

---
+++ ShowRegistryKey/ShowRegistryKey/Form1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShowRegistryKey
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (var process in Process.GetProcessesByName("regedit")) 
+            {
+                process.Kill();
+            }
+            string path = @"HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor";
+            Registry.CurrentUser.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Applets\Regedit").SetValue("LastKey", path);
+            Process.Start("regedit");
+        }
+    }
+}
