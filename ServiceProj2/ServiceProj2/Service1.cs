658b08921b84d45d9d5d59dad650555eb798e5db
Sabotage84 <sabotage84@mail.ru>
2021-08-10 17:01:25 +0300

    Add service

---
+++ ServiceProj2/ServiceProj2/Service1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceProj2
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnStart(string[] args)
+        {
+            while (true)
+            {
+                File.AppendAllText("Path", "SomeText");
+                await Task.Delay(500);
+            }
+            
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
