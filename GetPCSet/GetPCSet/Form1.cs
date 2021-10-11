9821c64418d7bfe14f4b9a6ef871d0951320b593
Sabotage84 <sabotage84@mail.ru>
2021-10-12 00:24:38 +0300

    ADD get pc set 2

--- GetPCSet/GetPCSet/Form1.cs
+++ GetPCSet/GetPCSet/Form1.cs
@@ -20,11 +20,31 @@ namespace GetPCSet
 
         private void button1_Click(object sender, EventArgs e)
         {
-            ManagementObjectSearcher mos = new ManagementObjectSearcher("root\\cimv2","select * from win32_videocontroller");
+            //Длинный вариант получаем видяху
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("select * from win32_videocontroller");
             ManagementObjectCollection moc = mos.Get();
             foreach (var item in moc)
             {
                 label1.Text = item.GetPropertyValue("Name").ToString();
+            }
+
+            label1.Text += "\n";
+
+            //Покороче получаем мать
+            foreach (var mo in new ManagementObjectSearcher("select * from win32_baseboard").Get())
+            {
+                label1.Text += $"{(string)mo["manufacturer"]} {(string)mo["product"]}";
+            }
+            label1.Text += "\n";
+
+            //Получаем монитор проверить на большом компе
+            foreach (var item in new ManagementObjectSearcher("root\\wmi", "select * from WmiMonitorID").Get())
+            {
+                foreach (var mo in (ushort[])item["UserFriendlyName"])
+                {
+                    label1.Text += (char)mo;
+                }
+
             }
         }
     }
