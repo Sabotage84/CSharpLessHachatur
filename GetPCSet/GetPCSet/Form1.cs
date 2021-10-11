f2089ac87daca63722df6d492045f4f162567b81
Sabotage84 <sabotage84@mail.ru>
2021-10-11 22:22:45 +0300

    ADD get pc set 1

--- GetPCSet/GetPCSet/Form1.cs
+++ GetPCSet/GetPCSet/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Management;
 
 namespace GetPCSet
 {
@@ -15,6 +16,16 @@ namespace GetPCSet
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("root\\cimv2","select * from win32_videocontroller");
+            ManagementObjectCollection moc = mos.Get();
+            foreach (var item in moc)
+            {
+                label1.Text = item.GetPropertyValue("Name").ToString();
+            }
         }
     }
 }
