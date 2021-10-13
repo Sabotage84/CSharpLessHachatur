95906ec2c02e9f5144f207121e6750d9752ad5c4
Sabotage84 <sabotage84@mail.ru>
2021-10-14 00:05:48 +0300

    ADD animatoin 2

--- AnimationWinForms/AnimationWinForms/Form1.Designer.cs
+++ AnimationWinForms/AnimationWinForms/Form1.Designer.cs
@@ -29,13 +29,71 @@ namespace AnimationWinForms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(351, 423);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 400);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel2.Location = new System.Drawing.Point(406, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(400, 400);
+            this.panel2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 114);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1\r\nlabel1\r\n\r\nlabel1\r\nlabel1\r\nlabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(823, 471);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
--- AnimationWinForms/AnimationWinForms/Form1.cs
+++ AnimationWinForms/AnimationWinForms/Form1.cs
@@ -12,9 +12,39 @@ namespace AnimationWinForms
 {
     public partial class Form1 : Form
     {
+        bool expectation;
+        bool old;
         public Form1()
         {
             InitializeComponent();
+            panel2.MouseEnter += async(s, a) =>
+            {
+                while (!expectation && panel2.Location.X > panel1.Location.X + 10)
+                {
+                    expectation = true;
+                    await Task.Delay(1);
+                    panel2.Location = old? new Point(panel2.Location.X - 10, panel2.Location.Y):
+                    new Point(panel2.Location.X - panel2.Location.X/10, panel2.Location.Y);
+                    expectation = false;
+                }
+            };
+
+            panel1.MouseEnter += async (s, a) =>
+            {
+                while (!expectation && panel2.Location.X < panel1.Width)
+                {
+                    expectation = true;
+                    await Task.Delay(1);
+                    panel2.Location = old? new Point(panel2.Location.X + 10, panel2.Location.Y):
+                    new Point(panel2.Location.X + (panel2.Width- panel2.Location.X)/8+2 , panel2.Location.Y);
+                    expectation = false;
+                }
+            };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            old = !old;
         }
     }
 }
